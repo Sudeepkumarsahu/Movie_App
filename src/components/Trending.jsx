960de83d475690932b5d912104cf7053ca08181b
sudeep <sahusudeepkumar3@gmail.com>
2025-12-14 12:42:49 +0530

    infinte scrolling

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -5,28 +5,54 @@ import Dropdown from "./templates/Dropdown";
 import axios from "../utils/axios";
 import Cards from "./templates/Cards";
 import Loading from "./Loading";
-
+import InfiniteScroll from 'react-infinite-scroll-component';
 const Trending = () => {
   const navigate = useNavigate();
   const [category, setcategory] = useState("all");
   const [duration, setduration] = useState("day");
-  const [trending, settrending] = useState(null);
+  const [trending, settrending] = useState([]);
+  const [page, setpage] = useState(1);
+  const [hasMore, sethasMore] = useState(true);
   const getTrending = async () => {
     try {
-      const { data } = await axios.get(`/trending/${category}/${duration}`);
-      settrending(data.results);
+      const { data } = await axios.get(
+        `/trending/${category}/${duration}?page=${page}`
+    );
+      if(data.results.length > 0){
+          settrending((prevState) => [...prevState, ...data.results]);
+          setpage(page +1);
+        
+      }else{
+sethasMore(false);
+      }
+    //   settrending(data.results);
+      console.log(data);
+      
     } catch (error) {
       console.log("Error: ", error);
     }
   };
-  useEffect(() => {
-    getTrending();
-  }, [category, duration]);
-  console.log(trending);
 
-  return trending ? (
-    <div className=" px-[3%] w-screen h-screen overflow-hidden overflow-y-auto">
-      <div className=" w-full  flex items-center justify-between ">
+const refreshHandler = () => {
+    if(trending.length === 0){
+        getTrending();
+    }else{
+        setpage(1);
+        settrending([]);
+        getTrending();
+    }
+};
+
+
+
+  useEffect(() => {
+refreshHandler();
+  }, [category, duration]);
+//   console.log(trending);
+
+  return trending.length > 0 ? (
+    <div className="  w-screen h-screen ">
+      <div className="px-[5%] w-full  flex items-center justify-between ">
         <h1 className=" text-2xl text-zinc-400 font-semibold">
           <i
             onClick={() => navigate(-1)}
@@ -42,7 +68,16 @@ const Trending = () => {
         </div>
       </div>
 
+        <InfiniteScroll
+        dataLength={trending.length}
+        next={getTrending}
+        hasMore={hasMore}
+        loader={<h1>loadin  g....</h1>}
+        >
+
       <Cards data={trending} title={category} />
+        </InfiniteScroll>
+
     </div>
   ): <Loading />
 };
